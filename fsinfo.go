b2ab16fd68de0cfc0127956b8757bd3758be95ea
scott <scott.gillespie@r1soft.com>
2016-06-28 09:20:20 -0500

    cleanup

--- fsinfo.go
+++ fsinfo.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+    //"fmt"
     "log"
     "syscall"
     "gopkg.in/gomail.v2"
@@ -15,7 +15,6 @@ type DiskStatus struct {
 
 }
 
-//func DiskUsage(path string) (disk DiskStatus) {
 func DiskUsage(path string) (disk DiskStatus) {
 
 	fs  := syscall.Statfs_t{}
@@ -29,32 +28,31 @@ func DiskUsage(path string) (disk DiskStatus) {
 	disk.Free = fs.Bfree  * uint64(fs.Bsize)
 	disk.Used = disk.All - disk.Free
 
-	fmt.Printf("used:\t %.2f GB\n", (float64(disk.Used)/1024/1024/1024))
-
-    fmt.Printf("%.2f\n", disk.Used)
-
     return
+
 }
 
 func main() {
 
     // sbjenkins-root 
     disk := DiskUsage("/")
+    used := (float64(disk.Used) / float64(disk.Free) * 100.0)
+    //used := 99.0
+    threshhold := float64(95.0)
 
-    fmt.Printf("free:\t %.2f%% \n", float64(disk.Used) / float64(disk.Free) * 100.0)
+    if used >= threshhold {
+        m := gomail.NewMessage()
+        m.SetHeader("From", "scott.gillespie@r1soft.com")
+        m.SetHeader("To", "scott.gillespie@r1soft.com")
+        m.SetHeader("Subject", "SBJENKINS SHITTER'S FULL!")
+        m.SetBody("text/html", "jenkins-root SHITTER'S FULL!")
 
-    m := gomail.NewMessage()
-    m.SetHeader("From", "scott.gillespie@r1soft.com")
-    m.SetHeader("To", "scott.gillespie@r1soft.com")
-    m.SetHeader("Subject", "SBJENKINS SHITTER'S FULL!")
-    m.SetBody("text/html", "jenkins-root SHITTER'S FULL!")
+        d := gomail.NewDialer("smtp.office365.com", 587, "scott.gillespie@r1soft.com", "")
 
-    d := gomail.NewDialer("smtp.office365.com", 587, "scott.gillespie@r1soft.com", "k1lm3n0w!")
+        err := d.DialAndSend(m)
 
-    err := d.DialAndSend(m)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
+        if err != nil {
+            log.Fatal(err)
+        }
+    } 
 }
