6a09985a13aa012a813315555792c954dc9ea5fa
scott <scott.gillespie@r1soft.com>
2016-06-28 19:23:06 -0500

    updated fsinfo

--- fsinfo.go
+++ fsinfo.go
@@ -1,58 +1,45 @@
 package main
 
 import (
-    //"fmt"
-    "log"
-    "syscall"
-    "gopkg.in/gomail.v2"
+	"fmt"
+	"log"
+	"syscall"
+	"gopkg.in/gomail.v2"
 )
-
-type DiskStatus struct {
-
-	All  uint64 `json: "all"`
-	Used uint64 `json: "used"`
-	Free uint64 `json: "free"`
-
-}
-
-func DiskUsage(path string) (disk DiskStatus) {
-
-	fs  := syscall.Statfs_t{}
-	err := syscall.Statfs(path, &fs)
-
-	if err != nil {
-		return
-	}
-
-	disk.All  = fs.Blocks * uint64(fs.Bsize)
-	disk.Free = fs.Bfree  * uint64(fs.Bsize)
-	disk.Used = disk.All - disk.Free
-
-    return
-
-}
 
 func main() {
 
-    // sbjenkins-root 
-    disk := DiskUsage("/")
-    used := (float64(disk.Used) / float64(disk.Free) * 100.0)
-    //used := float64(100.0) 
-    threshhold := float64(95.0)
+	stat := syscall.Statfs_t{}
+	err  := syscall.Statfs("/", &stat)
 
-    if used >= threshhold {
-        m := gomail.NewMessage()
-        m.SetHeader("From", "noreply@r1soft.com")
-        m.SetHeader("To", "scott.gillespie@r1soft.com", "alex.vongluck@r1soft.com", "stan.love@r1soft.com")
-        m.SetHeader("Subject", "SBJENKINS root filesystem at or over 95% full.")
-        m.SetBody("text/html", "jenkins-root filesystem above 95% used. Please cleanup some old builds, if possible.")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-        d := gomail.NewDialer("smtp.office365.com", 587, "noreply@r1soft.com", "")
+	n := stat.Bfree - stat.Bavail
+	m := stat.Blocks
+	p := float64(n) / float64(m) * float64(100.0)
 
-        err := d.DialAndSend(m)
+	fmt.Printf("%.2f\n", p)
 
-        if err != nil {
-            log.Fatal(err)
-        }
-    } 
+	threshhold := float64(95.0)
+
+	//test := float64(77.0)
+
+	if p >= threshhold {
+		m := gomail.NewMessage()
+		m.SetHeader("From", "noreply@r1soft.com")
+		//m.SetHeader("To", "scott.gillespie@r1soft.com", "alex.vongluck@r1soft.com", "stan.love@r1soft.com")
+		m.SetHeader("To", "scott.gillespie@r1soft.com")
+		m.SetHeader("Subject", "SBJENKINS root filesystem at or over 95% full.")
+		m.SetBody("text/html", "jenkins-root filesystem above 95% used. Please cleanup some old builds, if possible.")
+
+		d := gomail.NewDialer("smtp.office365.com", 587, "noreply@r1soft.com", "")
+
+		err := d.DialAndSend(m)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} 
 }
