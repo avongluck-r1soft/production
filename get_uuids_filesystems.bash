dc108a7513b21e67481371dcf43050cfbc5bb16e
scott <scott.gillespie@r1soft.com>
2016-07-01 08:19:01 -0500

    get uuids and filesystems after a drive failure

---
+++ get_uuids_filesystems.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in $(cat ~/agents); do 
+	getDiskSafe $i; 
+done | grep "Could not read disk safe meta data for Disk Safe" -A1 -B6 > failed_agents.txt 2>&1
+
+awk '/information/ {print $6}' failed_agents.txt > uuids.txt
+awk '/storage0/ {print}' failed_agents.txt  | sed "s/'//g" > filesystems.txt
+paste -d',' uuids.txt filesystems.txt > uuids_filesystems.csv
+
--- mon.go
+++ mon.go
@@ -1,33 +1,32 @@
-// mon.go - golang simple monitor. Mongo like candy!
-
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
-	"log"
+	//"io/ioutil"
+	"log"	
+ 	"os"
+	//"strings"
 	"syscall"
 	"gopkg.in/gomail.v2"
 )
 
 func getNoreplyPassword() (pw string) {
-
-	file, err := ioutil.ReadFile(".noreplypw")
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println(string(file))
+	f, _ := os.Open("/home/scott/pw/.noreplypw")
+	scanner := bufio.NewScanner(f)
+	line := scanner.Text()
+	fmt.Println(line)
+	pw = line
 	return pw
 }
 
 
 func main() {
 
-	pw	:= getNoreplyPassword()
+	pw := getNoreplyPassword()
+	//pw := strings.Trim(getNoreplyPassword(), "\n")
 
+	fmt.Print("DEBUG\n")
 	fmt.Println(pw)
 
 	stat	:= syscall.Statfs_t{}
@@ -44,11 +43,11 @@ func main() {
 
 	fmt.Printf("%.2f\n", pct)
 
-	threshhold := float64(95.0)
+	//threshhold := float64(95.0)
 	//threshhold := float64(1.0)
 
-	if pct >= threshhold {
-		fmt.Printf("sending mail...\n"); 
+	//if pct >= threshhold {
+		fmt.Printf("sending mail...\n");
 		m := gomail.NewMessage()
 		m.SetHeader("From", "noreply@r1soft.com")
 		//m.SetHeader("To", "scott.gillespie@r1soft.com", "alex.vongluck@r1soft.com", "stan.love@r1soft.com")
@@ -56,12 +55,12 @@ func main() {
 		m.SetHeader("Subject", "SBJENKINS root filesystem at or over 95% full.")
 		m.SetBody("text/html", "jenkins-root filesystem above 95% used. Please cleanup some old builds, if possible.")
 
-		//d := gomail.NewDialer("smtp.office365.com", 587, "noreply@r1soft.com", "")
-		d := gomail.NewDialer("smtp.office365.com", 587, "noreply@r1soft.com", pw)
+		d := gomail.NewDialer("smtp.office365.com", 587, "noreply@r1soft.com", pw)	
+		//d := gomail.NewDialer("smtp.office365.com", 587, "noreply@r1soft.com", "NoR3plyR1$0ft")
 
-		err := d.DialAndSend(m)
-		if err != nil {
-			log.Fatal(err)
+		errn := d.DialAndSend(m)
+		if errn != nil {
+			log.Fatal(errn)
 		}
-	} 
+	//}
 }
