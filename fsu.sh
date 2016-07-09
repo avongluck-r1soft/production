3888582e8b14682be176b4b391ea8b375b6784bc
scott <scott.gillespie@r1soft.com>
2016-07-09 18:40:13 -0500

    fix some fings

--- fsu.sh
+++ fsu.sh
@@ -11,20 +11,22 @@
 FSGT80="filesystems_over_80.out"
 FSLT40="filesystems_under_40.out"
 
-# keep less than 10 files
-if [ $(ls fs_usage.out*|wc -l) -ge 10 ]; then
-	rm ./fs_usage.out*
-fi
+cleanup() {
+	# keep less than 10 files
+	if [ $(ls fs_usage.out*|wc -l) -ge 10 ]; then
+		rm ./fs_usage.out*
+	fi
 
-if [ $(ls|grep fs_usage >/dev/null 2>&1; echo $?) -eq 1 ]; then
-	echo "no fs_usage file exists. creating fs_usage.out0"
-	touch fs_usage.out0
-fi
+	if [ $(ls|grep fs_usage >/dev/null 2>&1; echo $?) -eq 1 ]; then
+		echo "no fs_usage file exists. creating fs_usage.out0"
+		touch fs_usage.out0
+	fi
 
-if [ ! -f "get_fs_pct.rb" ]; then
-	echo "missing get_fs_pct.rb script"
-	exit 42
-fi
+	if [ ! -f "get_fs_pct.rb" ]; then
+		echo "missing get_fs_pct.rb script"
+		exit 42
+	fi
+}
 
 get_fs_data() {
 	n=`ls|grep out|tail -1|sed 's/[a-zA-Z_.]//g'`
@@ -37,23 +39,31 @@ get_fs_data() {
 	sort -rk 2 > $FSLT40
 }
 
-if [ -f $FSGT80 ]; then
-	cp $FSGT80 $FSGT80.prev
-else 
-	touch $FSGT80
-fi
+check_prev() {
+	if [ -f $FSGT80 ]; then
+		cp $FSGT80 $FSGT80.prev
+	else 
+		touch $FSGT80
+	fi
 
-if [ -f $FSLT40 ]; then
-	cp $FSLT40 $FSLT40.prev
-else
-	touch $FSLT40
-fi
+	if [ -f $FSLT40 ]; then
+		cp $FSLT40 $FSLT40.prev
+	else
+		touch $FSLT40
+	fi
+}
 
+report() {
+	echo "*** Filesystems at or over 80% ***"
+	cat $FSGT80
+	diff $FSGT80 $FSGT80.prev --suppress-common-lines
+
+	echo "*** Filesystems at or under 40% ***"
+	cat $FSLT40
+	diff $FSLT40 $FSLT40.prev --suppress-common-lines
+}
+
+cleanup
 get_fs_data
-echo "*** Filesystems at or over 80% ***"
-cat $FSGT80
-diff $FSGT80 $FSGT80.prev --suppress-common-lines
-
-echo "*** Filesystems at or under 40% ***"
-cat $FSLT40
-diff $FSLT40 $FSLT40.prev --suppress-common-lines
+check_prev
+report
