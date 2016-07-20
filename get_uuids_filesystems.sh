3246308bca24888ceb4651967912bd8ddddfcf20
scott <scott.gillespie@r1soft.com>
2016-07-19 21:18:40 -0500

    cleanup

--- get_uuids_filesystems.sh
+++ get_uuids_filesystems.sh
@@ -3,7 +3,7 @@
 # get_filesystems_uuids.bash - quick and dirty csv builder to run after a drive failure.
 
 for i in $(cat ~/agents); do
-        getDiskSafe $i;
+        getDiskSafe $i
 done | grep "Could not read disk safe meta data for Disk Safe" -A1 -B6 > failed_agents.txt 2>&1
 
 awk '/information/ {print $6}' failed_agents.txt > uuids.txt
