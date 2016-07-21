2ac90f44747c4a86c53524e5fec3239ab7c79ce8
scott <scott.gillespie@r1soft.com>
2016-07-21 18:53:22 -0500

    fix softlayer network configuration pull script

--- get_softlayer_conf.sh
+++ get_softlayer_conf.sh
@@ -16,22 +16,36 @@ fi
 slcli hardware detail $HOSTNAME > tmp
 
 PUBLIC_IP=$(grep public_ip tmp | awk '{print $2}')
+PUBLIC_TSUBNET=$(echo $PUBLIC_IP| awk -F"." '{print $1"."$2"."$3}')
+PUBLIC_SUBNET=$(slcli subnet list | grep $PUBLIC_TSUBNET | awk '{print $2}') 
 PRIVATE_IP=$(grep private_ip tmp | awk '{print $2}')
-TSUBNET=$(echo $PUBLIC_IP| awk -F"." '{print $1"."$2"."$3}')
-SUBNET=$(slcli subnet list | grep $TSUBNET | awk '{print $2}') 
 
-slcli subnet detail $SUBNET > tmp
+slcli subnet detail $PUBLIC_SUBNET > tmp
 
-GATEWAY=$(grep gateway tmp | awk '{print $2}')
-BROADCAST=$(grep broadcast tmp | awk '{print $2}')
+PUBLIC_GATEWAY=$(grep gateway tmp | awk '{print $2}')
+PUBLIC_BROADCAST=$(grep broadcast tmp | awk '{print $2}')
+PUBLIC_NETMASK=$(ipcalc $PUBLIC_SUBNET |grep Netmask | awk '{print $2}')
 
-NETMASK=$(ipcalc $SUBNET |grep Netmask | awk '{print $2}')
+PRIVATE_TSUBNET=$(echo $PRIVATE_IP| awk -F"." '{print $1"."$2"."$3}')
+PRIVATE_SUBNET=$(slcli subnet list | grep $PRIVATE_TSUBNET | awk '{print $2}') 
+
+slcli subnet detail $PRIVATE_SUBNET > tmp
+
+PRIVATE_GATEWAY=$(grep gateway tmp | awk '{print $2}')
+PRIVATE_BROADCAST=$(grep broadcast tmp | awk '{print $2}')
+PRIVATE_NETMASK=$(ipcalc $PRIVATE_SUBNET |grep Netmask | awk '{print $2}')
+
 
 echo "public ip: $PUBLIC_IP"
+echo "public subnet: $PUBLIC_SUBNET"
+echo "public netmask: $PUBLIC_NETMASK"
+echo "public gateway: $PUBLIC_GATEWAY"
+echo "public broadcast: $PUBLIC_BROADCAST"
+
+echo ""
+
 echo "private ip: $PRIVATE_IP"
-echo "subnet: $SUBNET"
-echo "netmask: $NETMASK"
-echo "gateway: $GATEWAY"
-echo "broadcast: $BROADCAST"
-
-
+echo "private subnet: $PRIVATE_SUBNET"
+echo "private netmask: $PRIVATE_NETMASK"
+echo "private gateway: $PRIVATE_GATEWAY"
+echo "private broadcast: $PRIVATE_BROADCAST"
