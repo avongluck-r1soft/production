ff689a396000f03b5376dff81f23e6d8feba8660
scott <scott.gillespie@r1soft.com>
2016-08-16 15:51:26 -0500

    start on service monitoring fxn

--- monitoring/c247mon.py
+++ monitoring/c247mon.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 import contextlib
+import commands
 import time
 import socket
 import multiprocessing
@@ -41,12 +42,13 @@ def main():
     while running:
 
         now = str(datetime.now())
-        print("\n########### " + now + " ###########\n")
-        print("hostname        = " + hostname)
-        print("ip_address      = " + ip_addr)
-        print("check_cpu       = " + str(check_cpu(MAX_HIGH_CPU, HIGH_CPU_COUNT)))
-        print("check_swapspace = " + str(check_swapspace(MAX_USED_SWAP)))
-        print("check_diskspace = " + str(check_diskspace(MAX_DISKSPACE_PCT)))
+        print("\n############### " + now + " ###############\n")
+        print("hostname              = " + hostname)
+        print("ip_address            = " + ip_addr)
+        print("check_cpu             = " + str(check_cpu(MAX_HIGH_CPU, HIGH_CPU_COUNT)))
+        print("check_swapspace       = " + str(check_swapspace(MAX_USED_SWAP)))
+        print("check_diskspace       = " + str(check_diskspace(MAX_DISKSPACE_PCT)))
+        print("check_service_running = " + str(check_service_running("r1rm")))
 
         time.sleep(60)
 
@@ -103,12 +105,22 @@ def check_diskspace(MAX_DISKSPACE_PCT):
                 DF_OUTPUT[fs_file] = block_usage_pct
       
                 if block_usage_pct > MAX_DISKSPACE_PCT:
-                    log_event("disk_space_/" + fs_spec + "DiskUsage high = " + block_usage_pct + " mount_point = " + fs_file)
+                    log_event("DEVOPS -- disk_space_/" + fs_spec + "DiskUsage high = " + block_usage_pct + " mount_point = " + fs_file)
 
                 print("%s\t%s\t%d%%" % (fs_spec, fs_file, block_usage_pct))
 
     return DF_OUTPUT
 
+def check_service_running(name):
+    
+    service_status = commands.getstatusoutput('service ' + name + ' status >/dev/null 2>&1; echo $?')
+    if service_status == 1:
+        log_event("DEVOPS -- Service " + name + " is down on " + hostname + ".")
+        log_event("Restarting " + name + " on " + hostname + ".")
+
+    msg = name + " is up"
+
+    return msg
                  
 if __name__ == "__main__":
     main()
