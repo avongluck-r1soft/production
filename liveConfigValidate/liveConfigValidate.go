ce7c7f9658ebdc8e2133c68d1c8996daa6fcc920
U-MSI\scott <scott.layne.gillespie@gmail.com>
2016-09-16 18:57:34 -0500

    update

--- liveConfigValidate/liveConfigValidate.go
+++ liveConfigValidate/liveConfigValidate.go
@@ -47,13 +47,13 @@ func equalJSON(s1, s2 string) (bool, error) {
 	var o2 interface{}
 
 	var err error
+
 	err = json.Unmarshal([]byte(s1), &o1)
 	if err != nil {
 		return false, fmt.Errorf("Error mashalling string 1 :: %s", err.Error())
 	}
 
 	err = json.Unmarshal([]byte(s2), &o2)
-
 	if err != nil {
 		return false, fmt.Errorf("Error mashalling string 2 :: %s", err.Error())
 	}
