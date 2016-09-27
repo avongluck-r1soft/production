92afb2b4a6908bb13d813beab3b561443677d9df
scott <scott.gillespie@r1soft.com>
2016-09-27 15:18:01 -0500

    +

---
+++ DriveSerials/listDrives.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func getDriveLocations() []string {
+	cmd := "storcli /c0/eall/sall show all|awk '/Device attributes/ {print $2}'"
+	res, err := exec.Command("bash","-c",cmd).Output()
+	if err != nil {
+		fmt.Sprintf("Failed to exec command: %s", cmd)
+	}
+	s := strings.Fields(fmt.Sprintf("%s", res))
+	return s
+}
+
+func getDriveSerialNums() []string {
+	cmd := "storcli /c0/eall/sall show all|awk '/SN =/ {print $3}'"
+	res, err := exec.Command("bash","-c",cmd).Output()
+	if err != nil {
+		fmt.Sprintf("Failed to exec command: %s", cmd)
+	}
+	s := strings.Fields(fmt.Sprintf("%s", res))
+	return s
+}
+
+func main() {
+	locations := getDriveLocations()
+	serials   := getDriveSerialNums()
+
+	for i := range locations {
+		fmt.Printf("%s\t%s\n", locations[i], serials[i])
+	}
+}
+
