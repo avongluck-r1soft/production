9545fccba1308e4ebc85664c60e9adfc9350c010
scott <scott.gillespie@r1soft.com>
2016-10-13 11:26:06 -0500

    find a dicksafe

---
+++ findDiskSafe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PASS=$1
+DSID=$2
+
+if [[ -z $2 ]]; then
+	echo "usage: $0 <passwd> <diskSafeID>"
+	exit -1
+fi
+
+for host in $(awk '/csbm/ {print $2}' /etc/hosts); do
+	data=$(sshpass -p$PASS ssh -o StrictHostKeyChecking=no continuum@$host "echo $PASS | sudo -S find /storage* -name $DSID -exec du -sh {} \;" 2>/dev/null) 
+	echo "$host $data"
+done
