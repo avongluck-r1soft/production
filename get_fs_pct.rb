d9a568ed582cc1a38c9cfe06251ef3362450c0f1
scott <scott.gillespie@r1soft.com>
2016-10-19 10:03:44 -0500

    rename

--- get_fs_pct.rb
+++ get_fs_pct.rb
@@ -4,11 +4,11 @@ require 'rubygems'
 require 'net/ssh'
 
 def get_password()
-  pass = ''
-  File.open("/home/scott/scripts/.pass/list", "r").each do |line|
-    pass = line.split[1]
-  end
-  return pass
+	pass = ''
+	File.open("/home/scott/scripts/.pass/list", "r").each do |line|
+		pass = line.split[1]
+	end
+	return pass
 end
 
 user = 'continuum'
@@ -18,17 +18,17 @@ pass = get_password()
 @cmd = "for i in {1..9}; do df -h /storage0$i|awk -v MYHOST=$(hostname) '/storage/ {print MYHOST\" \"$5\" \"$6}'; done"
 
 def get_fs_usage(host, user, pass)
-  puts host.chomp + " " + user + " " +pass
-  hn = host.chomp
-  Net::SSH.start(hn, user, :password => pass) do |ssh|
-    result = ssh.exec!(@cmd)
-    puts result
-  end
+	puts host.chomp + " " + user + " " +pass
+	hn = host.chomp
+	Net::SSH.start(hn, user, :password => pass) do |ssh|
+		result = ssh.exec!(@cmd)
+		puts result
+	end
 end
 
 
 HOSTS = '/home/scott/production/hosts'
 
 File.open("#{HOSTS}", "r").each do |host|
-  get_fs_usage(host, user, pass)
+	get_fs_usage(host, user, pass)
 end
