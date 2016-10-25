4382c6af2fb2dbf98750dcf071ef643996e17026
scott <scott.gillespie@r1soft.com>
2016-10-25 14:58:56 -0500

    left justify / cleanup output

--- dns_a_lookup.pl
+++ dns_a_lookup.pl
@@ -32,9 +32,12 @@ for (my $i = 0; $i < @hosts; $i++) {
 	my $s = lookup($hosts[$i]); 
 	my @data = split / /, $s;
 	if ($addresses[$i] == $data[1]) {
-		print "\033[92mOK   - softlayer: $hosts[$i] $addresses[$i] \t\t dns: $s\n"; 
+		#print "\033[92mOK   - softlayer: $hosts[$i] $addresses[$i] \t\t dns: $s\n"; 
+		printf "\033[92mOK   - softlayer: %-20s %-20s\tdns: %-40s\n", $hosts[$i], $addresses[$i], $s; 
+	
 	} else {
-		print "\033[91mWARN - softlayer: $hosts[$i] $addresses[$i] \t\t dns: $s\n"; 
+		#print "\033[91mWARN - softlayer: $hosts[$i] $addresses[$i] \t\t dns: $s\n"; 
+		printf "\033[91mWARN - softlayer: %-20s %-20s\tdns: %-40s\n", $hosts[$i], $addresses[$i], $s; 
 	}
 	print "\033[0m";
 }
