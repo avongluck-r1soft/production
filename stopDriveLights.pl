faaded51852a8c7fb59c4f3e54e31229ead2afb8
scott <scott.gillespie@r1soft.com>
2016-11-09 17:22:14 -0600

    tabs rule

--- stopDriveLights.pl
+++ stopDriveLights.pl
@@ -6,31 +6,31 @@ use strict;
 use warnings;
 
 sub get_drives {
-    my @eids_slots; 
-    open my $in, "storcli /c0 show all|" or die "cannot open $!"; 
-    while (<$in>) {
-        next if $_ !~ /^[0-9]:/; 
-        my @line = split;
-        my $eid_slot = $line[0];
-        if ($eid_slot) {
-            push @eids_slots, $eid_slot; 
-        }
-    }
-    return @eids_slots;
+	my @eids_slots; 
+	open my $in, "storcli /c0 show all|" or die "cannot open $!"; 
+	while (<$in>) {
+		next if $_ !~ /^[0-9]:/; 
+		my @line = split;
+		my $eid_slot = $line[0];
+		if ($eid_slot) {
+			push @eids_slots, $eid_slot; 
+		}
+	}
+	return @eids_slots;
 }
 
 sub stop_lights {
 
-    my @drives = get_drives();
+	my @drives = get_drives();
 
-    foreach my $drive (@drives) {
-        my @line = split ':', $drive;
-        my $eid  = $line[0];
-        my $slot = $line[1];
-        system("storcli /c0 /e$eid /s$slot stop locate"); 
-    }
+	foreach my $drive (@drives) {
+		my @line = split ':', $drive;
+		my $eid  = $line[0];
+		my $slot = $line[1];
+		system("storcli /c0 /e$eid /s$slot stop locate"); 
+	}
 }
 
 sub main() {
-    stop_lights(); 
+	stop_lights(); 
 }
