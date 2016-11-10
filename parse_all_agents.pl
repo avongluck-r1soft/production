33454471a4dd1aee3dbd9fd792518300396cc780
scott <scott.gillespie@r1soft.com>
2016-11-10 09:12:45 -0600

    up

--- parse_all_agents.pl
+++ parse_all_agents.pl
@@ -6,6 +6,7 @@ use List::Util qw(reduce);
 
 my @g;
 my @t;
+my @k;
 
 my $file = 'all_agents.csv';
 open my $f, "<",  $file, or die "cannot open: $!"; 
@@ -13,6 +14,12 @@ while (<$f>) {
 	my @line = split ','; 
 	push @g, $line[1], if $line[1] =~ /G/;
 	push @t, $line[1], if $line[1] =~ /T/; 
+	push @k, $line[1], if $line[1] =~ /K/; 
+}
+
+sub ktotal {
+	my @digits = map { (my $x = $_) =~ tr/K//d; $x} @k;
+	return reduce { $a + $b } @digits;
 }
 
 sub gtotal {
@@ -25,11 +32,16 @@ sub ttotal {
 	return reduce { $a + $b } @digits;
 }
 
+sub kb_to_mb {
+	my $k = shift;
+	return $k /= 1024;
+}
+
 sub gb_to_tb {
 	my $g = shift;
 	return $g /= 1024;
 }
 
-my $grand_total = gb_to_tb(gtotal()) + ttotal(); 
+my $grand_total = gb_to_tb(kb_to_mb(ktotal())) + gb_to_tb(gtotal()) + ttotal(); 
 printf "%.f TiB\n", $grand_total;
 
