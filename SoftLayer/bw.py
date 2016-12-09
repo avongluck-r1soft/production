e81972dc2ee1daeb3877b667e840888591ca6966
scott <scott.gillespie@r1soft.com>
2016-12-09 10:33:29 -0600

    now total it

--- SoftLayer/bw.py
+++ SoftLayer/bw.py
@@ -3,6 +3,8 @@
 import SoftLayer
 import pprint
 
+total_out = []
+
 def getPublicBandwidth():
 
     threshhold = 1000.0 
@@ -11,15 +13,16 @@ def getPublicBandwidth():
     pp = pprint.PrettyPrinter(indent=2)
     theMask = "mask[outboundPublicBandwidthUsage]"
     result = client['SoftLayer_Account'].getHardware()
-    print "server_name,public_out\n"
-        
+
     for server in result:
 
         serverInfo = client['SoftLayer_Hardware_Server'].getObject(id=server['id'],mask=theMask)
         pubout = float(serverInfo.get('outboundPublicBandwidthUsage',0.0))
 
 	if pubout > threshhold:
-            print(serverInfo['fullyQualifiedDomainName'] + ","),
-	    print("Warning! " + str(pubout))
+            total_out.append(pubout)
+            print(serverInfo['fullyQualifiedDomainName'] + ", "),
+	    print(str(pubout))
 	
 getPublicBandwidth()
+print "total: " + str(sum(total_out))
