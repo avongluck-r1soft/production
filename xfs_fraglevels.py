02fb6b62d54283f395cc6de7fb36cf9fc8aa6291
scott <scott.gillespie@r1soft.com>
2016-12-14 11:42:10 -0600

    xfs fragmentation level report script

---
+++ xfs_fraglevels.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+import operator
+import os
+import smtplib
+import subprocess
+import sys
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+from subprocess import Popen,PIPE,STDOUT
+
+def runCommand(str):
+    process = Popen(args=str, stdout=PIPE, shell=True)
+    return process.communicate()[0]
+
+def runXfsFragCheck():
+    runCommand("./get_fragmentation_levels > fragmentation_levels.txt 2>&1")
+    
+def getSMTPPassword():
+    credentials = {}
+    with open('../.noreplypw','r') as f:
+        for line in f:
+            user, pw = line.strip().split(':')
+
+    return pw
+
+
+def emailReport(RECIPIENTS):
+    fromaddr = 'noreply@r1soft.com'
+    toaddr = RECIPIENTS
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = 'XFS - systems with high XFS filesystem fragmentation levels.'
+
+    body = 'XFS - systems with high XFS frag levels (see attached).'
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = 'fragmentation_levels.txt'
+
+    attachment = open(filename, 'r')
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename= %s' % filename)
+    msg.attach(part)
+
+    pw = getSMTPPassword()
+
+    server = smtplib.SMTP('smtp.office365.com', 587)
+    server.starttls()
+    server.login(fromaddr, pw) 
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+    
+    attachment.close()
+
+
+def main():
+    runXfsFragCheck()
+    RECIPIENTS = 'scott.gillespie@r1soft.com,alex.vongluck@r1soft.com,stan.love@r1soft.com'
+    #RECIPIENTS = 'scott.gillespie@r1soft.com'
+    emailReport(RECIPIENTS)
+
+    
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
+
+
